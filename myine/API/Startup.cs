0ccd4456bf8dd1f76bb0a2a414b4b4d15f46531c
Mazdak <nazemi.works@gmail.com>
2020-03-01 12:16:38 +0330

    make db with ef

--- myine/API/Startup.cs
+++ myine/API/Startup.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API {
@@ -26,9 +27,11 @@ namespace API {
         // dependency injection container
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
+
             services.AddDbContext<DataContext> (opt => {
                 opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
+
             services.AddControllers ();
         }
 
