1e1c8c59312a7b9010ae813443b68f912778f463
Mazdak <nazemi.works@gmail.com>
2020-03-01 12:17:29 +0330

    make api work

--- myine/Persistence/DataContext.cs
+++ myine/Persistence/DataContext.cs
@@ -4,7 +4,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistence {
     public class DataContext : DbContext {
-        public DataContext (DbContextOptions<DataContext> options) : base (options) { }
+
         public DbSet<Value> Values { get; set; }
+
+        public DataContext (DbContextOptions<DataContext> options) : base (options) { }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder) 
+        {
+            modelBuilder  
+                .Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "value 101"},
+                    new Value { Id = 2, Name = "value 102"},
+                    new Value { Id = 3, Name = "value 103"}
+                );
+    }
+
     }
 }
